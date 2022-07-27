726f50d4ac9e486b3e817ad5f5456db36cd90c52
Jacek Pietrzak <wiewiex@gmail.com>
2022-07-27 22:15:38 +0200

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "../styles/Header.scss";
+import SingleContactLink from './SingleContactLink';
+
+export default function Header() {
+  return (
+    <header className='headerContainer'>
+        <div className='textContainer'>
+            <h1>Jacek Pietrzak</h1>
+            <h2>Junior Frontend / React Developer</h2>
+        </div>
+        <div className='contactDetails'>
+            <SingleContactLink content="wiewiex@gmail.com" icon="mail.png"/>
+            <SingleContactLink content="788433833" icon="phone.png"/>
+            <SingleContactLink content="Kraków" icon="address.png"/>
+            <SingleContactLink content="linkedin.com/in/wiewiex" icon="linkedin.png"/>
+            <SingleContactLink content="github.com/wiewiex" icon="github.png"/>
+        </div>        
+        <img className='profilowe' alt='profilowe' src='profilowe.png'/>
+    </header>
+  )
+}
---
+++ src/components/SingleContactLink.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SingleContactLink({ content, icon }) {
+  return (
+    <a href="/" className='singleContactLink'>
+        <img className='icon' alt="contact link icon" src={icon}/>
+        <p>{content}</p>
+    </a>
+  )
+}
